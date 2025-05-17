36966881aef57f66ad6554eae6e178b839e39c29
Wellington <welltecnic@hotmail.com>
2025-05-17 11:04:05 -0300

    16-JS-Criando o SlideShow Dinâmico

--- js/script.js
+++ js/script.js
@@ -16,4 +16,34 @@ function toggleMenu(){
         if(e.target.classList.contains('item-menu')){
             toggleMenu();
         }
-    })
+ })
+
+//DECLARANDO UM ARRAY DE IMAGENS
+
+let imagens=[
+    'images/index/banner.jpg',
+    'images/index/energia1.jpg',
+    'images/index/energia2.jpg',
+    'images/index/energia3.jpg',
+];
+
+//DECLARANDO AS VARIAVEIS
+let i=0;
+let tempo =3000;
+const hero = document.querySelector('.hero');
+
+//CRIANDO A FUNÇÃO DO SLIDESHOW 
+
+function slideShow(){
+    if(hero){
+        hero.style.backgroundImage=`var(--gradient), url('${imagens[i]}')`
+    }
+    i++;
+    if(i == imagens.length){
+        i = 0;
+    }
+    setTimeout('slideShow()',tempo);
+}
+slideShow();
+
+
